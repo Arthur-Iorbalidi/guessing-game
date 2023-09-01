f02fb2d6b5caba42b1de0d953b5671f54e7dea2b
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2023-09-01 16:25:35 +0300

    init: make task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minRange = 0;
+        this.maxRange = 0;
+        this.guessNumber = 0;
+    }
 
     setRange(min, max) {
-
+        this.minRange = min;
+        this.maxRange = max;
     }
 
     guess() {
-
+        this.guessNumber = Math.round((this.minRange + this.maxRange) / 2);
+        return this.guessNumber;
     }
 
     lower() {
-
+        this.maxRange = this.guessNumber;
     }
 
     greater() {
-
+        this.minRange = this.guessNumber;
     }
 }
 
